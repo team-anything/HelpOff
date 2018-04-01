c50d22fdd636a471a1e494a39ef69c2cdc7be4c8
inishchith <inishchith@gmail.com>
2018-04-01 14:33:06 +0530

    update

--- App/app.py
+++ App/app.py
@@ -1,9 +1,12 @@
 import query_retreive as qr
 import retreiveData as rd
-import time
+import time,pickle
 
 if __name__=="__main__":
     previous = []
+    file = open("./aipc.pickle","rb")
+    placename = pickle.load(file)
+    file.close()
     while(1):
         response = qr.response()
         if(response != previous):
@@ -13,12 +16,12 @@ if __name__=="__main__":
             query = response[2]
             contact = response[3]   
             # pin code conv .
-            placename = "Dahisar,Mumbai" # 
-            data = rd.retreive_area(placename,query)
+            print(placename[str(pincode)])
+            data = rd.retreive_area(placename[str(pincode)],query)
             print(data)
             if data != None:
                 text = data[0]+"\n"+data[1]+"\n"+data[2]
-                qr.sendSMS(str(contact)[2:],text)
-
+                #qr.sendSMS(str(contact)[2:],text)
+                print("Message Sent : app.py")
         previous  = response    
         time.sleep(5)
